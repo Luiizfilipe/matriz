4373640c85eecb00f5f702a57c4dbffb77537c40
Luiizfilipe <ghostf3477@gmail.com>
2024-08-02 19:41:29 -0300

    TRABALHO PRÁTICO 1

---
+++ TRABALHO PRÁTICO 1/adivinha/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Dificuldade do jogo!: Fácil (F), Médio (M), Difícil (D)" );
+            string dificuldade = Console.ReadLine().ToUpper();
+
+            int numeroMinimo, numeroMaximo, vidas;
+
+        switch (dificuldade)
+        {
+            case "F":
+                numeroMinimo = 1;
+                numeroMaximo = 10;
+                vidas = 3;
+                break;
+                    case "M":
+                    numeroMinimo = 1;
+                    numeroMaximo = 50;
+                    vidas = 4;
+                    break;
+                         case "D":
+                            numeroMinimo = 1;
+                            numeroMaximo = 100;
+                            vidas = 5;
+                            break;
+            default:
+                Console.WriteLine("Dificuldade inválida.");
+                return;
+        }
+
+
+        Random random = new Random();
+                    int numeroSecreto = random.Next(numeroMinimo, numeroMaximo + 1);
+
+        while (vidas > 0)   
+            
+            {
+            Console.WriteLine($"Você tem {vidas} vidas. Tente adivinhar o número entre {numeroMinimo} e {numeroMaximo}:");
+            if (int.TryParse(Console.ReadLine(), out int palpite))
+            {
+                if (palpite < numeroMinimo || palpite > numeroMaximo)
+                {
+                    Console.WriteLine($"Por favor, insira um número entre {numeroMinimo} e {numeroMaximo}.");
+                    continue;
+                }
+
+                    if (palpite == numeroSecreto)
+                    {
+                        Console.WriteLine("Parabéns! Você adivinhou o número. ");
+                        return;
+                }
+                else
+                {
+                            vidas--;
+                            if (vidas > 0)
+                            {
+                                Console.WriteLine("Errado! Tente novamente. ");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Você perdeu. O número era {numeroSecreto}. ");
+                            }
+                }
+            }
+            else
+            {
+                    Console.WriteLine("Entrada inválida. Por favor, insira um número. ");
+            }
+        }
+    }
+}
---
+++ TRABALHO PRÁTICO 1/jogodaforca/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] palavras = { "cachorro", "gato", "elefante", "girafa", "leao" };
+            Random random = new Random();
+                string palavraSecreta = palavras[random.Next(palavras.Length)];
+
+            char[] palavraAdivinhada = new string('_', palavraSecreta.Length).ToCharArray();
+                List<char> letrasUsadas = new List<char>();
+
+            int tentativas = palavraSecreta.Length + 5; // Número de tentativas
+
+        while (tentativas > 0)
+        {
+            Console.Clear();
+            Console.WriteLine($"Palavra: {new string(palavraAdivinhada)}");
+            Console.WriteLine($"Letras usadas: {string.Join(", ", letrasUsadas)}");
+            Console.WriteLine($"Tentativas restantes: {tentativas}");
+            Console.Write("Digite uma letra: ");
+            char letra;
+
+            try
+            {
+                letra = Convert.ToChar(Console.ReadLine().ToLower());
+            }
+            catch
+            {
+                Console.WriteLine("Por favor, insira uma única letra. ");
+                continue;
+            }
+
+            if (letrasUsadas.Contains(letra))
+            {
+                Console.WriteLine("Você já usou essa letra. Tente outra. ");
+                continue;
+            }
+
+            letrasUsadas.Add(letra);
+
+            if (palavraSecreta.Contains(letra))
+            {
+                for (int i = 0; i < palavraSecreta.Length; i++)
+                {
+                    if (palavraSecreta[i] == letra)
+                    {
+                        palavraAdivinhada[i] = letra;
+                    }
+                }
+            }
+            else
+            {
+                tentativas--;
+            }
+
+            if (new string(palavraAdivinhada) == palavraSecreta)
+            {
+                    Console.WriteLine($"Parabéns! Você adivinhou a palavra: {palavraSecreta}");
+                    return;
+                }
+        }
+
+            Console.WriteLine($"Você perdeu!!! A palavra era.: {palavraSecreta}");
+    }
+}
+
